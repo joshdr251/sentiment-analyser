b669855ef78b9fee067b11c3737b28c34b59ec22
joshdr251 <jonathan.schader@gmx.de>
2026-08-13 14:09:21 +0200

    Initial commit

---
+++ model_training.py
@@ -0,0 +1,60 @@
+
+import pandas as pd
+import unicodedata
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# load file
+while True:
+    try:
+        filepath_input = "imdb_dataset_50000_cleaned.csv" #input("Input filepath: ")
+        df = pd.read_csv(filepath_input)
+        break
+    except:
+        if FileNotFoundError:
+            print("File not found, try again.")
+            continue
+
+
+# separate features x and target variable y
+x = df["review"]
+y = df["sentiment"]
+
+# dividing in training and test data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
+# stratify ensures that training data is 50/50 (positive/negative)
+
+# creating vectorizer
+vectorizer = TfidfVectorizer(max_features=50000)  # stop_words removes words with little meaning
+x_train_vec = vectorizer.fit_transform(x_train)  # learns from the data
+x_test_vec = vectorizer.transform(x_test)  # only using, not learning
+
+# training the model
+model = LogisticRegression(max_iter=5000)
+model.fit(x_train_vec, y_train)
+
+# predicting and evaluation
+y_pred = model.predict(x_test_vec)
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print()
+print(classification_report(y_test, y_pred))
+print()
+print("Confusion Matrix: ")
+print(confusion_matrix(y_test, y_pred))
+
+
+# testing area
+print("Input sentences:")
+
+rows = []
+
+while True:
+    user_input = input("> ")
+    if user_input == "":
+        break
+    rows.append(user_input)
+    print(rows)
+    break
+
